a2325f59b9c515d8eda00a34f69c25d5072123c9
Aaron Liu <aaron.j.liu@gmail.com>
2022-05-18 15:44:56 -0400

    Adjusted box sizes to be proportional to grid... Removed box borders Added red coloring to boxes when mouse is clicked Added clear button that doesn't work...yet

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+function mouseDown(currentBox, colorChoice) {
+    currentBox.style.backgroundColor = colorChoice;
+    isMouseDown = 1;
+}
 
+function mouseUp() {
+    isMouseDown = 0;
+}
 
 function createGrid(size) {
-    const grid = document.querySelector('.grid-container');
     for(let i=0; i<size; i++) {
         const row = document.createElement('div');
         row.classList.add('grid-row');
-        // row.style.cssText('')
+        row.style.cssText = 'height: ' + 100/size + '%;';
         for(let j=0; j<size; j++) {
             const box = document.createElement('div');
             box.classList.add('grid-box');
+            box.style.cssText = 'width: ' + 100/size + '%;';
+            box.addEventListener('mousedown', function() {
+                mouseDown(box, 'red');
+            });
+            box.addEventListener('mouseover', function() {
+                if(isMouseDown) {
+                    mouseDown(box, 'red');
+                }
+            });
             row.appendChild(box);
         }
         grid.appendChild(row);
@@ -16,4 +31,7 @@ function createGrid(size) {
     
 }
 
+const grid = document.querySelector('.grid-container');
+let isMouseDown = 0;
 createGrid(16);
+grid.addEventListener('mouseup', mouseUp);
