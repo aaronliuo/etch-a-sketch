c32238105cdbe90a8236856903c6f17104b4c1f9
Aaron Liu <aaron.j.liu@gmail.com>
2022-05-18 18:14:08 -0400

    Added color pallete section... Added 10 colors that the user can select and switch between Added functional slider that updates the size of grid Added button that adjusts dimensions of the grid Added color and customized buttons and slider

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function mouseDown(currentBox, colorChoice) {
-    currentBox.style.backgroundColor = colorChoice;
+function mouseDown(currentBox) {
+    currentBox.style.backgroundColor = selectedColor;
     isMouseDown = 1;
 }
 
@@ -17,11 +17,11 @@ function createGrid(size) {
             box.classList.add('grid-box');
             box.style.cssText = 'width: ' + 100/size + '%;';
             box.addEventListener('mousedown', function() {
-                mouseDown(box, 'red');
+                mouseDown(box);
             });
             box.addEventListener('mouseover', function() {
                 if(isMouseDown) {
-                    mouseDown(box, 'red');
+                    mouseDown(box);
                 }
             });
             row.appendChild(box);
@@ -38,9 +38,62 @@ function clearGrid() {
     });
 }
 
+function resetGrid(size) {
+    const boxes = document.querySelectorAll('.grid-box');
+    const rows = document.querySelectorAll('.grid-row');
+    boxes.forEach((box) =>  {
+        box.remove();
+    });
+    rows.forEach((row) =>  {
+        row.remove();
+    });
+    createGrid(size);
+}
+
+function createColors() {
+    const colorPallete = document.querySelector('.color-pallete');
+    const colors = ['white', 'black', 'red', 'orange', 'yellow', 
+        'green', 'blue', 'purple', 'pink', 'grey'];
+
+
+    for(let i=0; i<5; i++) {
+        const row = document.createElement('div');
+        row.classList.add('color-row');
+        for(let j=0; j<2; j++) {
+            const box = document.createElement('div');
+            box.classList.add('color-box');
+            box.style.backgroundColor = colors[(i*2)+j];
+            box.addEventListener('click', function() {
+                selectedColor = colors[(i*2)+j];
+            });
+            row.appendChild(box);
+        }
+        colorPallete.appendChild(row);
+    }
+}
+
+//Initializes 16x16 grid and adds necessary event listeners
 const grid = document.querySelector('.grid-container');
+const body = document.querySelector('body');
 const clearGridButton = document.querySelector('.clear-grid');
 let isMouseDown = 0;
 createGrid(16);
-grid.addEventListener('mouseup', mouseUp);
+body.addEventListener('mouseup', mouseUp);
 clearGridButton.addEventListener('click', clearGrid);
+
+//Initializes all the colors
+let selectedColor = 'white';
+createColors();
+
+//Sets up slider input and output display
+const slider = document.querySelector('.slider');
+const sliderOutput = document.querySelector('.slide-output');
+slider.oninput = function() {
+    sliderOutput.innerHTML = (this.value + ' x ' + this.value);
+}
+
+//Applies new grid dimensions and clears all solors
+const resetGridButton = document.querySelector('.reset-grid');
+resetGridButton.addEventListener('click', function() {
+    resetGrid(slider.value);
+})
